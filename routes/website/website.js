ce298f47e91350a1e33501899122ff8a6c749b7e
PhiHoangN <phihoang17052003@gmail.com>
2024-07-15 21:37:11 +0700

    List-Order

--- routes/website/website.js
+++ routes/website/website.js
@@ -4,12 +4,14 @@ const authRouter = require("./auth/auth");
 const homeRouter = require("./revenue/revenue");
 const productRouter = require("./products/products");
 const brandRouter = require("./brand/brand");
+const orderRouter = require("./order/order");
 
 
 router.use("/auth", authRouter);
 router.use("/home", homeRouter);
 router.use("/brand", brandRouter);
 router.use("/product", productRouter);
+router.use("/order", orderRouter);
 
 
 module.exports = router;
