3ec3b6b788a1d6cfeebf8666e4b481170815dab5
haufpt <haundps26258@fpt.edu.vn>
2024-07-21 18:13:04 +0700

    Write Api Get List Cart

--- routes/api/api.js
+++ routes/api/api.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const authRouter = require("./auth/auth");
 const brandRouter = require("./brand/brand");
 const productRouter = require("./product/product");
+const cartRouter = require("./cart/cart");
 
 router.use("/auth", authRouter);
 
 router.use("/brand", brandRouter);
 
 router.use("/product", productRouter);
+
+router.use("/cart", cartRouter);
 
 module.exports = router;
--- service/cart/cart.js
+++ service/cart/cart.js
@@ -1,7 +1,45 @@
 const cartItemsModel = require("../../model/cart_item");
+const mongoose = require("mongoose");
 
 const findCart = async (filter) => {
   return await cartItemsModel.findOne(filter);
+};
+
+const getListCart = async (accountId) => {
+  console.log("[CartService] getListCart: ", accountId);
+  try {
+    const pipeline = [
+      {
+        $match: { accountId: new mongoose.Types.ObjectId(accountId) },
+      },
+      {
+        $lookup: {
+          from: "products",
+          localField: "productId",
+          foreignField: "_id",
+          as: "product",
+        },
+      },
+      {
+        $unwind: {
+          path: "$product",
+          preserveNullAndEmptyArrays: false,
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          product: 1,
+          quantity: 1,
+        },
+      },
+    ];
+
+    var carts = await cartItemsModel.aggregate(pipeline);
+    return carts;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const findByIdAndUpdate = async (cartId, newInfomation) => {
@@ -22,4 +60,5 @@ module.exports = {
   findByIdAndUpdate,
   addCart,
   deleteCartById,
+  getListCart,
 };
