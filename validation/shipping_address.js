ab03be7f36ebc33753bc5e4e56b1fd1e602b0aff
haufpt <haundps26258@fpt.edu.vn>
2024-07-23 00:54:40 +0700

    Write Api Delete Shipping Address

--- validation/shipping_address.js
+++ validation/shipping_address.js
@@ -2,6 +2,7 @@ const Joi = require("joi");
 
 class ShippingAddressValidation {
   static createNewShippingAddress = Joi.object({
+    title: Joi.string().max(255).required(),
     receiver: Joi.string().max(255).required(),
     address: Joi.string().required(),
     phoneNumber: Joi.string().max(255).trim().required(),
@@ -9,6 +10,7 @@ class ShippingAddressValidation {
   });
 
   static updateShippingAddress = Joi.object({
+    title: Joi.string().max(255).optional(),
     receiver: Joi.string().max(255).optional(),
     address: Joi.string().optional(),
     phoneNumber: Joi.string().max(255).trim().optional(),
