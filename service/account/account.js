38c4e8ec58611894ab16c1380348b46dd189a900
ManhDO213 <163748515+CongChien0905@users.noreply.github.com>
2024-07-24 02:08:10 +0700

    Call api getlist and getDetailOrder

--- service/account/account.js
+++ service/account/account.js
@@ -13,8 +13,31 @@ const findByIdAndUpdate = async (idAccount, newInfomation) => {
   return await accountModel.findByIdAndUpdate(idAccount, newInfomation);
 };
 
+const findAccountById = async (idAccount) => {
+  return await accountModel.findById(idAccount);
+};
+
+const getAllAcountUser = async ({ page = 1, limit = 10 } = {}) => {
+  try {
+    const skip = (page - 1) * limit;
+    const accounts = await accountModel.find({ role: 'CLIENT' }).skip(skip).limit(limit);
+    const totalAccount = await accountModel.countDocuments({ role: 'CLIENT' });
+
+    return {
+      accounts: accounts,
+      currentPage: page,
+      totalPages: Math.ceil(totalAccount / limit),
+      totalAccount,
+    };
+  } catch (error) {
+    throw error;
+  }
+};
+
 module.exports = {
   findAccount,
   addAccount,
   findByIdAndUpdate,
+  getAllAcountUser,
+  findAccountById
 };
---
+++ service/order/orderService.js
@@ -0,0 +1,137 @@
+const orderModel = require("../../model/order");
+const accountModel = require("../../model/account");
+const producttemModel = require("../../model/product");
+const cartItemsModel = require("../../model/cart_item");
+const mongoose = require("mongoose");
+const order_item = require("../../model/order_item");
+
+const getListOrder = async () => {
+  try {
+    const pipeline = [
+      {
+        $match: {
+          status: { $ne: "PROCESSING" }
+        }
+      },
+      {
+        $project: {
+          _id: 1,
+          code: 1,
+          orderDate: 1,
+          paymentMethod: 1,
+          status: 1,
+        },
+      },
+    ];
+
+    return await orderModel.aggregate(pipeline);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getListOrderByStatus = async (status) => {
+  const pipeline = [
+    {
+      $match: {
+        status: status,
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        code: 1,
+        orderDate: 1,
+        paymentMethod: 1,
+        status: 1,
+      },
+    },
+  ];
+  return await orderModel.aggregate(pipeline);
+};
+
+const getListOrderById = async (orderID) => {
+  try {
+    console.log(`[orderService] getListOrderById: orderId -> ${orderID}`);
+    const pipeline = [
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(orderID),
+        },
+      },
+      {
+        $lookup: {
+          from: "accounts",
+          localField: "accountId",
+          foreignField: "_id",
+          as: "accounts"
+        },
+      },
+      {
+        $unwind: {
+          path: "$accounts",
+          preserveNullAndEmptyArrays: false,
+        },
+      },
+      {
+        $lookup: {
+          from: "order_items",
+          localField: "_id",
+          foreignField: "orderId",
+          as: "order_items",
+          pipeline: [
+            {
+              $lookup: {
+                from: "products",
+                localField: "productId",
+                foreignField: "_id",
+                as: "products",
+              },
+            },
+            {
+              $unwind: {
+                path: "$products",
+                preserveNullAndEmptyArrays: false,
+              },
+            },
+          ],
+        },
+      },
+      {
+        $lookup: {
+          from: "shipping_addresses",
+          localField: "accounts._id",
+          foreignField: "accountId",
+          as: "shipping_addresses",
+        },
+      },
+      {
+        $unwind: {
+          path: "$shipping_addresses",
+          preserveNullAndEmptyArrays: false,
+        },
+      },
+      {
+        $group: {
+          _id: "$_id",
+          orders: { $first: "$$ROOT" },
+        },
+      },
+    ];
+
+    const orderData = await orderModel.aggregate(pipeline);
+    console.log(`[orderService] getListOrderById: data -> ${orderData}`);
+    if (orderData.length != 1) {
+      return null;
+    }
+    return orderData[0];
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = {
+  getListOrder,
+  getListOrderByStatus,
+  getListOrderById,
+};
