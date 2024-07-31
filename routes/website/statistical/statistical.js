9a9877d7d11bfad1cc593bc6ef4fe0b0ff9e38d9
PhiHoangN <phihoang17052003@gmail.com>
2024-07-31 23:06:41 +0700

    Front-End-Statistical

---
+++ routes/website/statistical/statistical.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var router = express.Router();
+var statisticalController = require("../../../controller/admin/statistical/statisticalController");
+const {checkRole} = require("../../../middleware/auth");
+const { AccountRoleEnum, FileTypeEnum } = require("../../../common/enum");
+
+router.get(
+  "/statisticalStore",
+  checkRole([AccountRoleEnum.ADMIN]),
+  statisticalController.statisticalStore
+);
+
+module.exports = router;
